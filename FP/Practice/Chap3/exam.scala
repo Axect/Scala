783d188cb3114a8f6522518ba224bbfcefc9cd99
Axect <edeftg@gmail.com>
2017-11-09 14:57:57 +0900

    Implement sum

--- FP/Practice/Chap3/exam.scala
+++ FP/Practice/Chap3/exam.scala
@@ -2,7 +2,14 @@ import fpinscala.datastructures._
 
 object Exam {
   def main(args: Array[String]): Unit = {
-    val ans = List.x
-    println(s"The answer is ${ans}")
+    val ans = sum(x)
+    println(s"The result of sum is ${ans}")
   }
+
+  def sum(ar: List[Int]): Int = ar match {
+    case Nil => 0
+    case Cons(x, xs) => x + sum(xs)
+  }
+
+  val x = List(1,2,3,4,5)
 }
