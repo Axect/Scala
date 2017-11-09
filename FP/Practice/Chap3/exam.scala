147684155fc6797a2413da05b4a6efcaf74a0d8b
Axect <edeftg@gmail.com>
2017-11-09 14:52:42 +0900

    Fixed! & String Interpolation

--- FP/Practice/Chap3/exam.scala
+++ FP/Practice/Chap3/exam.scala
@@ -1,17 +1,8 @@
 import fpinscala.datastructures._
 
-object Example {
+object Exam {
   def main(args: Array[String]): Unit = {
-    println(answer)
-  }
-
-  val answer = {
-    val x = List(1,2,3,4,5) match {
-      case Cons(x, Cons(2, Cons(4,_))) => x
-      case Nil => 42
-      case Cons(x, Cons(y, Cons(3, Cons(4,_)))) => x + y
-      case Cons(h, t) => h + sum(t)
-      case _ => 101
-    }
+    val ans = List.x
+    println(s"The answer is ${ans}")
   }
 }
