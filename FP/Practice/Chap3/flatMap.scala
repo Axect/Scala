32bef9523bbae78286b637ca22ee9ecdbb2a40ea
Axect <edeftg@gmail.com>
2017-11-20 16:32:41 +0900

    FlatMap Finish

--- FP/Practice/Chap3/flatMap.scala
+++ FP/Practice/Chap3/flatMap.scala
@@ -2,10 +2,18 @@ import fpinscala.datastructures._
 
 object FlatMap {
   def main(args: Array[String]): Unit = {
-    println()
+    println(flatMap(temp)(x => List(x, x)))
   }
 
   def flatMap[A, B](l: List[A])(f: A => List[B]): List[B] = l match {
-    
+    case Nil => Nil
+    case Cons(x, xs) => concat(f(x), flatMap(xs)(f))
   }
+
+  def concat[A](l1: List[A], l2: List[A]): List[A] = l1 match {
+    case Nil => l2
+    case Cons(x1, xs1) => Cons(x1, concat(xs1, l2))
+  }
+
+  val temp = List(1,2,3)
 }
