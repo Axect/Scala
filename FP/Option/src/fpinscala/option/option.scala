2be339106e1e93ea0fbec6267956f63dfbf7850e
Tae Geun, Kim <edeftg@gmail.com>
2017-11-24 01:27:48 +0900

    Option Trait
    Flatmap is too hard

--- FP/Option/src/fpinscala/option/option.scala
+++ FP/Option/src/fpinscala/option/option.scala
@@ -1,5 +1,20 @@
 package fpinscala.option
 
-sealed trait Option[+A]
-case class Some[+A](get: A) extends Option[A]
+sealed trait Option[+A] {
+  def map[B](f: A => B): Option[B] = this match {
+    case None => None
+    case Some(x) => Some(f(x))
+  }
+
+  // TODO #1
+  def getOrElse[B >: A](default: => B): B = this match { // >: means super type & => B means lazy B
+    case None => default
+    case Some(a) => a // What is B?!
+  }
+
+  // TODO #2
+  def flatMap[B](f: A => Option[B]): Option[B] =
+    map(f) getOrElse None // map = Option[B] => ...?
+}
 case object None extends Option[Nothing]
+case class Some[+A](get: A) extends Option[A]
