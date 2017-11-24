8e40357306fce7de515a7fed1646706eb74c8d1f
Tae Geun, Kim <edeftg@gmail.com>
2017-11-24 19:29:15 +0900

    Option Trait
    Complete to understand FlatMap

--- FP/Option/src/fpinscala/option/option.scala
+++ FP/Option/src/fpinscala/option/option.scala
@@ -6,15 +6,19 @@ sealed trait Option[+A] {
     case Some(x) => Some(f(x))
   }
 
-  // TODO #1
   def getOrElse[B >: A](default: => B): B = this match { // >: means super type & => B means lazy B
     case None => default
     case Some(a) => a // What is B?!
   }
 
-  // TODO #2
   def flatMap[B](f: A => Option[B]): Option[B] =
-    map(f) getOrElse None // map = Option[B] => ...?
+    map(f) getOrElse None
+
+  def filter(f: A => Boolean): Option[A] = this match {
+    case Some(a) if f(a) => Some(a)
+    case Some(a) if !f(a) => None
+  }
 }
+
 case object None extends Option[Nothing]
 case class Some[+A](get: A) extends Option[A]
