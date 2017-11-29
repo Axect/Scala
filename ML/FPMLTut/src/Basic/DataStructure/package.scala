af633efbb2ea95a0dd35325e9bbbf84fe67342e2
Tae Geun, Kim <edeftg@gmail.com>
2017-11-29 09:56:34 +0900

    Add Print for DVector & DMatrix
    also add implicit transforms

--- ML/FPMLTut/src/Basic/DataStructure/package.scala
+++ ML/FPMLTut/src/Basic/DataStructure/package.scala
@@ -3,6 +3,7 @@ package Basic
 import scala.reflect.ClassTag
 
 package object DataStructure {
+  // Basic Type Aliases
   type XY = Tuple2[Double, Double]
   def XY(x: Double, y:Double) = Tuple2[Double, Double](x,y)
 
@@ -20,4 +21,15 @@ package object DataStructure {
 
   type DblMatrix = DMatrix[Double]
   def DblMatrix(x: DblVector*) = DMatrix[Double](x: _*)
+
+  def DVectorString[T:ClassTag](x: DVector[T]): String = s"DVector(${x.mkString(",")})"
+  def DMatrixString[T:ClassTag](m: DMatrix[T]): String = s"DMatrix(${m.map(x => DVectorString(x)).mkString("\n\t\t")})"
+
+  def printVector[T:ClassTag](x: DVector[T]) = println(DVectorString(x))
+  def printMatrix[T:ClassTag](m: DMatrix[T]) = println(DMatrixString(m))
+
+  // Implicit Transforms
+  implicit def int2Double(n: Int): Double = n.toDouble
+  implicit def vector2DblVector[T <% Double](x: DVector[T]): DblVector = x.map(elem => elem.toDouble)
+  implicit def double2DblVector(x: Double): DblVector = DblVector(x)
 }
--- ML/FPMLTut/src/test.scala
+++ ML/FPMLTut/src/test.scala
@@ -3,6 +3,9 @@ import Basic.DataStructure._
 object test {
   def main(args: Array[String]): Unit = {
     val x = DVector[Int](0, 1, 2, 3, 4)
-    println(x.mkString(","))
+    printVector(x)
+
+    val m = DMatrix(DVector(1,2,3), DVector(4,5,6), DVector(7,8,9))
+    printMatrix(m)
   }
 }
